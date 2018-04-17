243c78ea4dcfaa70fc9b68ef22c9fd404136e688
YanKaiKai-17 <ykk_edu@163.com>
2018-04-17 22:08:13 +0800

    Initial commit

---
+++ src/main/java/com/sselab/ykk/DockerEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.sselab.ykk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DockerEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerEurekaServerApplication.class, args);
+	}
+}
